5d6c2c4263afaca974b046d336f1dd256f3806fb
Alicia Martin <anmartin114@gmail.com>
2020-05-15 21:42:07 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { AboutComponent } from "./about/about.component";
+import { MenuComponent } from "./menu/menu.component";
+import { OrderComponent } from "./order/order.component";
+import { ContactComponent } from "./contact/contact.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "homepage", pathMatch: "full" },
+  { path: "homepage", component: HomepageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "menu", component: MenuComponent },
+  { path: "order", component: OrderComponent },
+  { path: "contact", component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    MenuComponent,
+    OrderComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
