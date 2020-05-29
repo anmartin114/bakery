6f2f54e061af83cd61709056b9d29a21432b780f
Alicia Martin <anmartin114@gmail.com>
2020-05-28 21:18:25 -0400

    add contact page delete about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomepageComponent } from "./homepage/homepage.component";
-import { AboutComponent } from "./about/about.component";
 import { MenuComponent } from "./menu/menu.component";
 import { OrderComponent } from "./order/order.component";
 import { ContactComponent } from "./contact/contact.component";
@@ -9,14 +8,13 @@ import { ContactComponent } from "./contact/contact.component";
 const routes: Routes = [
   { path: "", redirectTo: "homepage", pathMatch: "full" },
   { path: "homepage", component: HomepageComponent },
-  { path: "about", component: AboutComponent },
   { path: "menu", component: MenuComponent },
   { path: "order", component: OrderComponent },
-  { path: "contact", component: ContactComponent }
+  { path: "contact", component: ContactComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { AboutComponent } from './about/about.component';
-import { MenuComponent } from './menu/menu.component';
-import { OrderComponent } from './order/order.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+
+import { MenuComponent } from "./menu/menu.component";
+import { OrderComponent } from "./order/order.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    AboutComponent,
     MenuComponent,
     OrderComponent,
-    ContactComponent
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
